feb4c7ae25a6992b9025e83bf852150ca565a301
007kimetsuu <austinalappat08@gmail.com>
2026-09-05 16:08:04 +0530

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -365,10 +365,9 @@ if (!summary) {
   }
 );
 
-const PORT = 5000;
 
-app.listen(PORT, () => {
-  console.log(
-    `Server running on http://localhost:${PORT}`
-  );
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
